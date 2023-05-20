0704e5c2e16f24d08082660385e3a8a29df5e510
Alejandro López Pérez <60120226+alejo117lp@users.noreply.github.com>
2023-05-19 21:04:13 -0500

    Alejo (#5)
    * GrowinggEffect
    
    * GrowingEffectP2
    
    * Grifo y plantas terminado
    
    * Last Settings

--- Assets/00_Alejo/Scripts/VFXController.cs
+++ Assets/00_Alejo/Scripts/VFXController.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using UnityEngine.UI;
 
 public class VFXController : MonoBehaviour
 {
     [SerializeField] VisualEffect visualEffect;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Color Settings")]
+    [SerializeField] private Slider red;
+    [SerializeField] private Slider green;
+    [SerializeField] private Slider blue;
+
+    public void ChangeLighting() {
+
+        Vector4 newColor = new (red.value, green.value, blue.value);
+        visualEffect.SetVector4("Lighting", newColor);
+        visualEffect.SetVector4("Flash", newColor);
+        visualEffect.SetVector4("Sparks", newColor);
     }
 
-    
+
 }
